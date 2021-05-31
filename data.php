bce7e68439f7fc3ad8ee4c1238c8e70c993ce498
Nii797 <Phanterasonpardus797@gmail.com>
2021-05-31 16:53:41 +0700

    update branch Data-Pengguna

--- data.php
+++ data.php
@@ -6,11 +6,9 @@
 require_once('drink.php');
 require_once('food.php');
 require_once('review.php');
+// new
+require_once('user.php');
 
-// warisan dari Menu child Food dan Drink
-
-// makanan 
-// tambah argument 'dingin' dan 'panas' untuk menambahkan type
 $juice = new Drink('JUS',6,'img/juice.png','dingin');
 $coffee = new Drink('KOPI',5,'img/coffee.png','panas');
 $thaitea = new Drink('THAI-TEA',3,'img/Thai-Tea.png','dingin');
@@ -22,8 +20,20 @@ $sate = new Food('SATE',6,'img/sate.png',1);
 
 $menus = array($juice, $coffee, $thaitea, $curry, $pasta, $sate);
 
-$review1 = new Review($juice->getName(),'Mantap');
-$review2 = new Review($curry->getName(),'Gurih nikmat dan sedap');
+// new menambahkan user
+$user1 = new User('Soni','Pria');
+$user1 = new User('Ririn','Perempuan');
+
+$users = array($user1, $user2);
+
+$review1 = new Review($juice->getName(), 'Yummy');
+$review2 = new Review($curry->getName(), 'Sangat sehat');
+$review3 = new Review($coffee->getName(), 'Wanginya harum');
+$review4 = new Review($pasta->getName(), 'Sausnya enak :)');
+$review5 = new Review($juice->getName(), 'Hanya jus jeruk biasa');
+$review6 = new Review($curry->getName(), 'Rasanya enak untuk harganya');
+$review7 = new Review($coffee->getName(), 'Kepahitannya cukup.');
+$review8 = new Review($pasta->getName(), 'Bahan yang digunakan berkualitas.');
 
 $reviews = array($review1, $review2);
 
